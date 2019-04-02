61692e5d0296b6950f0518427f077f68021bd004
Yi Zhang <zhang.yi11@husky.neu.edu>
2019-04-01 23:36:36 -0700

    A5

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('*', function (req, res) {
 
 server.listen( port , () => console.log('Running on port 3200'));
 
-const connectionString = 'mongodb://127.0.0.1:27017/webdev';
-//var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+//const connectionString = 'mongodb://127.0.0.1:27017/webdev';
+const connectionString = 'mongodb://heroku_jv88xmwz:aq1sw2de3fr4@ds115931.mlab.com:15931/heroku_jv88xmwz'
 let mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const client = mongoose.connect( connectionString, { useNewUrlParser: true });
